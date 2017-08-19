ce37d2160fccf6072c044d3a013c95054e30b768
kamelot43 <andrepud@mail.ru>
2017-08-19 22:10:51 +0300

    выполнено задание 4

--- js/map.js
+++ js/map.js
@@ -3,14 +3,16 @@ var PRICE_MIN = 1000;
 var PRICE_MAX = 1000000;
 var ROOMS_MIN = 1;
 var ROOMS_MAX = 5;
-var MIN_COORDINATE_X = 300 + PIN_WIDTH / 2;
-var MAX_COORDINATE_X = 900 + PIN_WIDTH / 2;
-var MIN_COORDINATE_Y = 100 + PIN_HEIGHT;
-var MAX_COORDINATE_Y = 500 + PIN_HEIGHT;
+var OFFERS_AMOUNT = 8;
 
 //координаты пина
 var PIN_WIDTH = 40;
 var PIN_HEIGHT = 40;
+
+var MIN_COORDINATE_X = 300 + PIN_WIDTH / 2;
+var MAX_COORDINATE_X = 900 + PIN_WIDTH / 2;
+var MIN_COORDINATE_Y = 100 + PIN_HEIGHT;
+var MAX_COORDINATE_Y = 500 + PIN_HEIGHT;
 
 var OFFER_HEADING = [
   "Большая уютная квартира",
@@ -63,11 +65,15 @@ function createOffers(param) {
   for (var i = 0; i < param; i++) {
     offers[i] = {
       author: {
-        avatar: "img/avatars/user" + "0[i]" + ".png"
+        avatar: "img/avatars/user0" + (i + 1) + ".png"
+      },
+      location: {
+        x: returnRandomValue(MIN_COORDINATE_X, MAX_COORDINATE_X),
+        y: returnRandomValue(MIN_COORDINATE_Y, MAX_COORDINATE_Y)
       },
       offer: {
         title: OFFER_HEADING[i],
-        address: this.location.x + "," + this.location.y,
+        adress: location.x + "," + location.y,
         price: returnRandomValue(PRICE_MIN, PRICE_MAX),
         type: getRandomElem(TYPE_ROOMS),
         rooms: returnRandomValue(ROOMS_MIN, ROOMS_MAX),
@@ -77,12 +83,32 @@ function createOffers(param) {
         features: getRandomArrays(1, 6, FEATURES),
         description: "",
         photos: []
-      },
-      location: {
-        x: returnRandomValue(MIN_COORDINATE_X, MAX_COORDINATE_X),
-        y: returnRandomValue(MIN_COORDINATE_Y, MAX_COORDINATE_Y)
       }
     };
   }
   return offers;
 }
+
+var tokyoPinMap = document.querySelector(".tokyo__pin-map");
+
+//функция, создающая div с заданными параметрами
+
+function createPins(arrays) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < arrays.length; i++) {
+    var pin = document.createElement("div");
+    var img = document.createElement("img");
+    pin.className = "pin";
+    img.className = "rounded";
+    img.width = "40";
+    img.height = "40";
+    img.src = arrays[i].author.avatar;
+    pin.style.left = arrays[i].location.x + "px";
+    pin.style.top = arrays[i].location.y + "px";
+    pin.appendChild(img);
+    fragment.appendChild(pin);
+  }
+  return fragment;
+}
+
+tokyoPinMap.appendChild(createPins(createOffers(OFFERS_AMOUNT)));

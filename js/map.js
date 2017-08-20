01f800fb7bc8f4fdc519366070c865f3d3fc64dc
kamelot43 <andrepud@mail.ru>
2017-08-20 11:08:12 +0300

    Код работает. Осталось проверить Travis

--- js/map.js
+++ js/map.js
@@ -3,16 +3,18 @@ var PRICE_MIN = 1000;
 var PRICE_MAX = 1000000;
 var ROOMS_MIN = 1;
 var ROOMS_MAX = 5;
+var GUESTS_MIN = 1;
+var GUESTS_MAX = 10;
 var OFFERS_AMOUNT = 8;
 
 //координаты пина
 var PIN_WIDTH = 40;
 var PIN_HEIGHT = 40;
 
-var MIN_COORDINATE_X = 300 + PIN_WIDTH / 2;
-var MAX_COORDINATE_X = 900 + PIN_WIDTH / 2;
-var MIN_COORDINATE_Y = 100 + PIN_HEIGHT;
-var MAX_COORDINATE_Y = 500 + PIN_HEIGHT;
+var MIN_COORDINATE_X = 300;
+var MAX_COORDINATE_X = 900;
+var MIN_COORDINATE_Y = 100;
+var MAX_COORDINATE_Y = 500;
 
 var OFFER_HEADING = [
   "Большая уютная квартира",
@@ -38,17 +40,15 @@ var FEATURES = [
 
 var TYPE_ROOMS = ["flat", "house", "bungalo"];
 
-var Template = document.querySelector('#lodge-template').content;
+var Template = document.querySelector("#lodge-template").content;
 
 function setTypeOfRooms(room) {
-  if (room === 'flat') {
-    return 'Квартира';
-  }
-  else if (room === 'bungalo') {
-    return 'Бунгало';
-  }
-  else {
-    return 'Дом';
+  if (room === "flat") {
+    return "Квартира";
+  } else if (room === "bungalo") {
+    return "Бунгало";
+  } else {
+    return "Дом";
   }
 }
 
@@ -63,6 +63,7 @@ function getRandomElem(array) {
   return array[rand];
 }
 
+// Полчить массив случайной длины
 function getRandomArrays(minValue, maxValue, array) {
   var randomLength = returnRandomValue(minValue, maxValue);
 
@@ -77,21 +78,22 @@ function getRandomArrays(minValue, maxValue, array) {
 function createOffers(param) {
   var offers = [];
   for (var i = 0; i < param; i++) {
+    var location = {
+      x: returnRandomValue(MIN_COORDINATE_X, MAX_COORDINATE_X) + PIN_WIDTH / 2,
+      y: returnRandomValue(MIN_COORDINATE_Y, MAX_COORDINATE_Y) + PIN_HEIGHT
+    };
     offers[i] = {
       author: {
         avatar: "img/avatars/user0" + (i + 1) + ".png"
       },
-      location: {
-        x: returnRandomValue(MIN_COORDINATE_X, MAX_COORDINATE_X),
-        y: returnRandomValue(MIN_COORDINATE_Y, MAX_COORDINATE_Y)
-      },
+      location: location,
       offer: {
         title: OFFER_HEADING[i],
         adress: location.x + "," + location.y,
         price: returnRandomValue(PRICE_MIN, PRICE_MAX),
         type: getRandomElem(TYPE_ROOMS),
         rooms: returnRandomValue(ROOMS_MIN, ROOMS_MAX),
-        guests: returnRandomValue(ROOMS_MIN, ROOMS_MAX), //не понятно сколько ?
+        guests: returnRandomValue(GUESTS_MIN, GUESTS_MAX),
         checkin: getRandomElem(TIME_REGISTRATION),
         checkout: getRandomElem(TIME_REGISTRATION),
         features: getRandomArrays(1, 6, FEATURES),
@@ -104,8 +106,10 @@ function createOffers(param) {
 }
 
 var tokyoPinMap = document.querySelector(".tokyo__pin-map");
-var offerDialog = document.querySelector(".offer-dialog");
-var dialogPanel = document.querySelector('.dialog__panel');
+var offerDialog = document.querySelector("#offer-dialog");
+var dialogPanel = document.querySelector(".dialog__panel");
+
+var dialogAvatar = document.querySelector(".dialog__title > img");
 
 //функция, создающая div с заданными параметрами
 
@@ -137,27 +141,52 @@ function createEmptySpan(arrays) {
   for (var i = 0; i < arrays.offer.features.length; i++) {
     var features = arrays.offer.features[i];
     var span = document.createElement("span");
-    span.className = 'feature__image feature__image--' + features;
+    span.className = "feature__image feature__image--" + features;
     fragment.appendChild(span);
   }
   return fragment;
 }
 
+var renderOffer = function(array) {
+  var OfferElement = Template.cloneNode(true);
 
-var renderOffer = function (array) {
-var OfferElement = Template.cloneNode(true);
-
-
-  OfferElement.querySelector('.lodge__title').textContent = array.offer.title;
-  OfferElement.querySelector('.lodge__address').textContent = array.offer.address;
-  OfferElement.querySelector('.lodge__price').textContent = array.offer.price + '&#x20bd;/ночь';
-  OfferElement.querySelector('.lodge__type').textContent = setTypeOfRooms(array.offer.type);
-  OfferElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для' + array.offer.guests + 'гостей в' + array.offer.rooms + 'комнатах';
-  OfferElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после' + array.offer.checkin + ', выезд до ' + array.offer.checkout;
-  OfferElement.querySelector('.lodge__features').appendChild(createEmptySpan(array));
-  OfferElement.querySelector('.lodge__description').textContent = array.offer.description;
+  OfferElement.querySelector(".lodge__title").textContent = array.offer.title;
+  OfferElement.querySelector(".lodge__address").textContent =
+    array.offer.adress;
+  OfferElement.querySelector(".lodge__price").innerHTML =
+    array.offer.price + "&#x20bd;/ночь";
+  OfferElement.querySelector(".lodge__type").textContent = setTypeOfRooms(
+    array.offer.type
+  );
+  OfferElement.querySelector(".lodge__rooms-and-guests").textContent =
+    "Для" +
+    " " +
+    array.offer.guests +
+    " " +
+    "гостей в" +
+    " " +
+    array.offer.rooms +
+    " " +
+    "комнатах";
+  OfferElement.querySelector(".lodge__checkin-time").textContent =
+    "Заезд после" +
+    " " +
+    array.offer.checkin +
+    ", выезд до " +
+    " " +
+    array.offer.checkout;
+  OfferElement.querySelector(".lodge__features").appendChild(
+    createEmptySpan(array)
+  );
+  OfferElement.querySelector(".lodge__description").textContent =
+    array.offer.description;
   return OfferElement;
 };
 
-var result = renderOffer(x[0]);
-offerDialog.replaceChild(result, dialogPanel);
+function PasteNewData(value) {
+  var result = renderOffer(value);
+  offerDialog.replaceChild(result, dialogPanel);
+  dialogAvatar.src = value.author.avatar;
+}
+
+PasteNewData(x[0]);
